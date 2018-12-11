263a271bd489f446998475ae25291881b53d5b30
joemissamore <joemissamore@live.com>
2018-12-11 03:41:28 +0000

    updated build name and removed hello dir

--- run_docker_django.sh
+++ run_docker_django.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-docker build . -t hello && \
-docker run -e AWS_SECRET_ACCESS_KEY -e AWS_ACCESS_KEY -p 8000:8000 hello
+docker build . -t django_app_docker && \
+docker run -e AWS_SECRET_ACCESS_KEY -e AWS_ACCESS_KEY -p 8000:8000 django_app_docker
 
